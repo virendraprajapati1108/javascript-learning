8deec76bc7a6e2b15b5a0af3d6ac7d24d2223771
virendraprajapati1108 <virendraprajapati1108@gmail.com>
2026-07-19 18:54:48 +0530

    Datatypes in JavaScript

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Virendra";
+let age = 24.5;
+let isStudent = false;
+let hobbies = ["Reading", "Traveling", "Coding"];
+let address = {
+  street: "Prajapati Vasm, Nana Kothasana",
+  city: "Satlasana",
+  state: "Gujarat",
+  country: "India"
+};
+let nullValue = null;
+let undefinedValue;
+
+console.log(typeof isStudent); // boolean
+console.log(typeof age); // number
+console.log(typeof name); // string
+console.log(typeof hobbies); // object
+console.log(typeof address); // object
+console.log(typeof nullValue); // object
+console.log(typeof undefinedValue); // undefined
